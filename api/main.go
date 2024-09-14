36fc239e848e8277c9dbc98f1e66d194ea1d46d1
Zachary Bray <zachbray9@knights.ucf.edu>
2024-09-14 01:47:34 -0400

    Used a different website for favicon generation
    Used a different website for favicon generation and updated api static file serving accordingly.

--- api/main.go
+++ api/main.go
@@ -33,17 +33,20 @@ func main() {
 
 		server.Static("/assets", "./wwwroot/assets")
 
-		// server.StaticFile("/android-chrome-192x192.png", "./wwwroot/android-chrome-192x192.png")
-		// server.StaticFile("/android-chrome-512x512.png", "./wwwroot/android-chrome-512x512.png")
-		// server.StaticFile("/apple-touch-icon.png", "./wwwroot/apple-touch-icon.png")
-		// server.StaticFile("/favicon-32x32.png", "./wwwroot/favicon-32x32.png")
-		// server.StaticFile("/favicon-16x16.png", "./wwwroot/favicon-16x16.png")
-		// server.StaticFile("/site.webmanifest", "./wwwroot/site.webmanifest")
+		server.StaticFile("/android-chrome-192x192.png", "./wwwroot/android-chrome-192x192.png")
+		server.StaticFile("/android-chrome-512x512.png", "./wwwroot/android-chrome-512x512.png")
+		server.StaticFile("/apple-touch-icon.png", "./wwwroot/apple-touch-icon.png")
+		server.StaticFile("/browserconfig.xml", "./wwwroot/browserconfig.xml")	
+		server.StaticFile("/favicon-16x16.png", "./wwwroot/favicon-16x16.png")
+		server.StaticFile("/favicon-32x32.png", "./wwwroot/favicon-32x32.png")
 		server.StaticFile("/favicon.ico", "./wwwroot/favicon.ico")
+		server.StaticFile("/mstile-150x150.png", "./wwwroot/mstile-150x150.png")
+		server.StaticFile("/safari-pinned-tab.svg", "./wwwroot/safari-pinned-tab.svg")
+		server.StaticFile("/site.webmanifest", "./wwwroot/site.webmanifest")
 
-		
-
-		server.StaticFile("/", "./wwwroot/index.html")
+		server.NoRoute(func (c *gin.Context){
+			c.File("./wwwroot/index.html")
+		})
 
 	}
 
