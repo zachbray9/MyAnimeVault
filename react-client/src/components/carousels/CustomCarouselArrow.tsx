4714e439b2c7b0ae6581a96bc8e567b1c5d2b85d
Zachary Bray <zachbray9@knights.ucf.edu>
2024-09-23 05:36:21 -0400

    Fixed Featured Carousel Arrow Spacing
    Made it so the featured carousel arrows go to the edge of the carousel and don't overlap the text.

--- react-client/src/components/carousels/CustomCarouselArrow.tsx
+++ react-client/src/components/carousels/CustomCarouselArrow.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { Box, useBreakpointValue } from "@chakra-ui/react";
+import { Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import { ArrowProps } from "react-multi-carousel";
 
 
@@ -35,9 +35,9 @@ export function CustomLeftCarouselArrow({ onClick }: ArrowProps) {
                 opacity: 0,
                 zIndex: -1
             }}
-            _hover={{ 
+            _hover={{
                 _before: {
-                    opacity: 1 
+                    opacity: 1
                 }
             }}
         >
@@ -46,7 +46,7 @@ export function CustomLeftCarouselArrow({ onClick }: ArrowProps) {
     )
 }
 
-export function CustomRightCarouselArrow({onClick }: ArrowProps) {
+export function CustomRightCarouselArrow({ onClick }: ArrowProps) {
     //needs to be the same as the padding of the top level stack in the AnimeCarousel component, but negative
     const responsivePadding = useBreakpointValue({
         base: '-1.25rem',
@@ -55,36 +55,84 @@ export function CustomRightCarouselArrow({onClick }: ArrowProps) {
 
     return (
         <Box
-        position="absolute"
-        right={responsivePadding}
-        top="0"
-        bottom="0"
-        display="flex"
-        paddingX='0.5rem'
-        alignItems="center"
-        justifyContent="center"
-        zIndex="10"
-        cursor="pointer"
-        onClick={onClick}
-        _before={{
-            content: '""',
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            bgGradient: 'linear(to-r, transparent, rgba(0, 0, 0, 0.80))',
-            transition: 'opacity 0.3s',
-            opacity: 0,
-            zIndex: -1
-        }}
-        _hover={{ 
-            _before: {
-                opacity: 1 
-            }
-        }}
+            position="absolute"
+            right={responsivePadding}
+            top="0"
+            bottom="0"
+            display="flex"
+            paddingX='0.5rem'
+            alignItems="center"
+            justifyContent="center"
+            zIndex="10"
+            cursor="pointer"
+            onClick={onClick}
+            _before={{
+                content: '""',
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                bgGradient: 'linear(to-r, transparent, rgba(0, 0, 0, 0.80))',
+                transition: 'opacity 0.3s',
+                opacity: 0,
+                zIndex: -1
+            }}
+            _hover={{
+                _before: {
+                    opacity: 1
+                }
+            }}
         >
             <ChevronRightIcon boxSize='2.5rem' color="white" />
         </Box>
+    )
+}
+
+export function CustomFeaturedLeftArrow({ onClick }: ArrowProps) {
+    return (
+        <IconButton
+            aria-label="left-arrow"
+            variant='ghost'
+            isRound
+            fontSize={['1.5rem', '2rem', '2.5rem']}
+            icon={<ChevronLeftIcon/>}
+            position="absolute"
+            left="5"
+            top="50%"
+            transform="translateY(-50%)"
+            onClick={onClick}
+            zIndex={1}
+            _hover={{
+                bg: 'blackAlpha.400'
+            }}
+            _active={{
+                bg: 'blackAlpha.600'
+            }}
+        />
+    )
+}
+
+export function CustomFeaturedRightArrow({ onClick }: ArrowProps) {
+    return (
+        <IconButton
+            aria-label="right-arrow"
+            variant='ghost'
+            isRound
+            fontSize={['1.5rem', '2rem', '2.5rem']}
+            icon={<ChevronRightIcon />}
+            position="absolute"
+            right="5"
+            top="50%"
+            transform="translateY(-50%)"
+            onClick={onClick}
+            zIndex={1}
+            _hover={{
+                bg: 'blackAlpha.400'
+            }}
+            _active={{
+                bg: 'blackAlpha.600'
+            }}
+        />
     )
 }
--- react-client/src/components/carousels/FeaturedCarousel.tsx
+++ react-client/src/components/carousels/FeaturedCarousel.tsx
@@ -8,6 +8,7 @@ import { NavLink } from "react-router-dom";
 import { Form, Formik } from "formik";
 import { useStore } from "../../stores/store";
 import { observer } from "mobx-react-lite";
+import { CustomFeaturedLeftArrow, CustomFeaturedRightArrow } from "./CustomCarouselArrow";
 
 interface Props {
     data: AniListAnime[]
@@ -23,7 +24,7 @@ export default observer(function FeaturedCarousel({ data }: Props) {
 
     return (
         <Box overflow='hidden'>
-            <Box overflow='visible'>
+            <Box overflow='visible' position='relative'>
                 <Carousel
                     responsive={featuredResponsive}
                     infinite={true}
@@ -31,11 +32,13 @@ export default observer(function FeaturedCarousel({ data }: Props) {
                     autoPlaySpeed={10000}
                     swipeable={true}
                     containerClass='carousel-container'
+                    customLeftArrow={<CustomFeaturedLeftArrow />}
+                    customRightArrow={<CustomFeaturedRightArrow />}
                 >
                     {data.map((anime) => (
                         <Box key={anime.id} bgImage={[anime.coverImage.large!, anime.bannerImage!]} position='relative' height={['60vh', null, '70vh']} backgroundSize='cover' backgroundPosition='center' display='flex' alignItems='center' justifyContent='center' overflow='visible'>
                             <Box position='absolute' top={0} bottom={0} left={0} right={0} background='rgba(0, 0, 0, 0.3)' bgGradient='linear(to-b, transparent, rgba(0, 0, 0, 1))' display='flex' alignItems={['end', 'center']} justifyContent={['center', null, 'start']} padding={{ base: '1rem', md: '8rem' }}>
-                                <Stack transform={[null, 'translateY(40%)']} alignItems={['center', null, 'start']} width='100%' maxWidth={['100%', null, '40vw']} gap={4} >
+                                <Stack transform={[null, 'translateY(30%)']} alignItems={['center', null, 'start']} width='100%' maxWidth={['100%', null, '40vw']} gap={4} >
                                     <Heading size={['lg', null, 'xl']} textAlign={['center', null, 'left']}>{anime.title.english ?? anime.title.romaji}</Heading>
                                     {anime.genres && <Text color='text.subtle'>{anime.genres.join(', ')}</Text>}
                                     <Text display={{ base: 'none', md: '-webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 4' }} overflow='hidden' textOverflow='ellipsis'>{stripHtml(anime.description!)}</Text>
