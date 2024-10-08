510eab0f190f4518be69f7ef121180f22740f739
Zachary Bray <zachbray9@knights.ucf.edu>
2024-10-08 19:16:15 -0400

    Fixed issue where CarouselCard hover overlay showed on mobile devices
    Fixed issue where carousel card hover overlay showed incorrectly on mobile devices. Switched from using OnMouseEnter and OnMouseLeave to _hover to control hover state.

--- react-client/src/components/carousels/CarouselCard.tsx
+++ react-client/src/components/carousels/CarouselCard.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, Icon, IconButton, Image, Stack, Text, Tooltip } from "@chakra-ui/react";
 import { AniListAnime } from "../../models/aniListAnime";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import { FaCheck, FaRegBookmark, FaStar } from "react-icons/fa6";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../stores/store";
@@ -12,7 +11,6 @@ interface Props {
 
 export default observer(function CarouselCard({ anime }: Props) {
     const {userStore} = useStore()
-    const [isHovered, setIsHovered] = useState(false)
 
     function stripHtmlTags(html: string): string {
         const div = document.createElement('div');
@@ -37,12 +35,12 @@ export default observer(function CarouselCard({ anime }: Props) {
     }
 
     return (
-        <Box as={NavLink} to={`/anime/${anime.id}/details`} key={anime.id} position='relative' gap={4} bg='surface.1' cursor='pointer' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
+        <Box as={NavLink} to={`/anime/${anime.id}/details`} key={anime.id} position='relative' gap={4} bg='surface.1' cursor='pointer' _hover={{'.overlay': {opacity: 1}}}>
             <Image src={anime.coverImage.large ?? undefined} width='100%' aspectRatio='2/3' objectFit='contain' />
 
             <Text fontSize='sm'>{anime.title.english ?? anime.title.romaji}</Text>
 
-            <Box position='absolute' top='-.5rem' bottom='-.5rem' right='-.5rem' left='-.5rem' opacity={isHovered ? 1 : 0} transition='opacity 0.2s ease'>
+            <Box className='overlay' position='absolute' top='-.5rem' bottom='-.5rem' right='-.5rem' left='-.5rem' opacity={0} transition='opacity 0.2s ease'>
                 <Image src={anime.coverImage.large} position='absolute' top={0} bottom={0} left={0} right={0} width='100%' height='100%' objectFit='cover' />
                 <Box position='absolute' top={0} bottom={0} left={0} right={0} bg='rgb(20, 21, 25, 0.9)'>
                     <Stack height='100%' justifyContent='space-between' padding='0.5rem'>
