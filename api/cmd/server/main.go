cbef0f8a49040fcfbf963727aca38d7f4999465d
Zachary Bray <zachbray9@knights.ucf.edu>
2025-07-07 23:11:31 -0400

    Attempted to fix static file serving bug
    Attempted to fix static file serving bug

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -31,28 +31,8 @@ func main() {
 
 	//initialize api endpoints
 	routes.InitRouter(server)
-
-	if os.Getenv("MODE") == "production" {
-		// server.Static("/assets", "./wwwroot/assets")
-		server.Static("/", "./wwwroot")
-
-		server.StaticFile("/android-chrome-192x192.png", "./wwwroot/android-chrome-192x192.png")
-		server.StaticFile("/android-chrome-512x512.png", "./wwwroot/android-chrome-512x512.png")
-		server.StaticFile("/apple-touch-icon.png", "./wwwroot/apple-touch-icon.png")
-		server.StaticFile("/browserconfig.xml", "./wwwroot/browserconfig.xml")
-		server.StaticFile("/favicon-16x16.png", "./wwwroot/favicon-16x16.png")
-		server.StaticFile("/favicon-32x32.png", "./wwwroot/favicon-32x32.png")
-		server.StaticFile("/favicon.ico", "./wwwroot/favicon.ico")
-		server.StaticFile("/mstile-150x150.png", "./wwwroot/mstile-150x150.png")
-		server.StaticFile("/safari-pinned-tab.svg", "./wwwroot/safari-pinned-tab.svg")
-		server.StaticFile("/site.webmanifest", "./wwwroot/site.webmanifest")
-
-		server.NoRoute(func(c *gin.Context) {
-			c.File("./wwwroot/index.html")
-		})
-
-		
-	}
+	routes.InitStaticRoutes(server)
+	
 
 	port := os.Getenv("PORT")
 	server.Run("0.0.0.0:" + port)
