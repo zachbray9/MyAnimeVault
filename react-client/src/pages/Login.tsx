5be3795a29ba3f9a0ce01df53dfd688d3fb3138d
Zachary Bray <zachbray9@knights.ucf.edu>
2025-08-16 02:13:59 -0400

    Redesigned login and register pages.
    Redesigned the login and register pages to match the new theme.

--- react-client/src/pages/Login.tsx
+++ react-client/src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import { observer } from "mobx-react-lite"
-import { Box, Button, Card, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react"
+import { Box, Button, Card, Flex, Heading, Image, Link, Stack, Text } from "@chakra-ui/react"
 import { Form, Formik } from "formik"
 import FormInput from "../components/common/form/FormInput"
 import { useStore } from "../stores/store"
 import * as Yup from "yup"
 import { NavLink } from "react-router-dom"
 import { Helmet } from "react-helmet-async"
+import Logo from "../assets/PlotArmorLogo.png"
 
 export default observer(function Login() {
     const { userStore } = useStore()
@@ -21,8 +22,8 @@ export default observer(function Login() {
                 <title>Suit up - Login to PlotArmor</title>
             </Helmet>
 
-            <Box width='100%' height='85svh' display='flex' justifyContent='center' alignItems='center' padding={['1.5rem', '1.75rem', '4rem']}>
-                <Card.Root bg="background.secondary" maxWidth='31rem' width='100%' padding={['1.25rem', '1.75rem', '2rem']}>
+            <Stack width='100%' height='85svh' display='flex' justifyContent='center' alignItems='center' padding={['1.5rem', '1.75rem', '4rem']}>
+                <Card.Root bg='transparent' border="none" maxWidth='31rem' width='100%' padding={['1.25rem', '1.75rem', '2rem']}>
                     <Formik
                         initialValues={{ email: '', password: '', error: null }}
                         onSubmit={(values, { setErrors }) => userStore.login(values).catch(() => setErrors({ error: "Your email or password is incorrect." }))}
@@ -30,14 +31,18 @@ export default observer(function Login() {
                     >
                         {({ handleSubmit, isSubmitting, errors }) => (
                             <Form onSubmit={handleSubmit} >
-                                <Card.Header>
-                                    <Heading size="3xl" textAlign="center">Log In</Heading>
+                                <Card.Header as={Stack} alignItems="center" textAlign="center" gap={2}>
+                                    <Image src={Logo} boxSize="75px" alt="PlotArmor Logo"/>
+                                    <Stack gap={1}>
+                                        <Heading size="3xl" textAlign="center">Welcome back</Heading>
+                                        <Text color="text.subtle">Sign in to suit up and get back to exploring.</Text>
+                                    </Stack>
                                 </Card.Header>
 
-                                <Card.Body as={Stack} gap={['1.5rem', '1.75rem', '2rem']}>
-                                    <FormInput name="email" placeholder="Email" variant='flushed' />
+                                <Card.Body as={Stack} gap={4}>
+                                    <FormInput name="email" placeholder="Email" bg="surface.sunken" rounded="lg"/>
 
-                                    <FormInput name="password" placeholder="Password" variant='flushed' hideable />
+                                    <FormInput name="password" placeholder="Password" bg="surface.sunken" rounded="lg" hideable />
                                 </Card.Body>
 
                                 <Card.Footer display='flex' flexDirection='column' justifyContent='start' alignItems='center' gap={['1.25rem', '1.75', '2rem']}>
@@ -45,12 +50,12 @@ export default observer(function Login() {
                                         {errors.error && <Text color='text.danger'>{errors.error}</Text>}
                                     </Box>
 
-                                    <Button type="submit" bg="interactive.primary" loading={isSubmitting} >Log In</Button>
+                                    <Button type="submit" bg="interactive.primary" color="text" w="100%" rounded="lg" _hover={{bg: "interactive.primary-hover"}} loading={isSubmitting} >Log In</Button>
 
-                                    <Flex gap={1}>
+                                    <Flex gap={1} color="text.subtle">
                                         <Text>No account?</Text>
                                         <NavLink to="/register">
-                                            <Link color='interactive.primary' _hover={{ color: 'text', borderColor: "text"}} transition='color 200ms'>Create One</Link>
+                                            <Text color='interactive.primary' _hover={{ color: 'interactive.primary-hover'}} transition='color 200ms' cursor="pointer">Create One</Text>
                                         </NavLink>
                                     </Flex>
                                 </Card.Footer>
@@ -59,7 +64,7 @@ export default observer(function Login() {
                     </Formik>
                 </Card.Root>
 
-            </Box>
+            </Stack>
         </>
     )
 })
